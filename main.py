063432d60580f0f00fbb247520c56f8b2c137eb8
Insaneovo <insaneovo@outlook.com>
2025-05-04 23:03:31 +0800

    修复错误

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import sys
 import traceback
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright , Playwright
 from config import get_config
 from utils import parse_to_playwright_cookies
 
 print('开始执行...')
 try:
     config = get_config()
-    playwright = sync_playwright()
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    context.add_cookies(parse_to_playwright_cookies(config['cookies']))
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context()
+        context.add_cookies(parse_to_playwright_cookies(config['cookies']))
 
-    page = context.new_page()
+        page = context.new_page()
 
-    page.goto("https://www.douyin.com/?recommend=1")
+        page.goto("https://www.douyin.com/?recommend=1")
 
-    print('等待弹窗1')
-    # 询问是否保存登陆信息 关闭
-    page.get_by_text("取消").click(timeout=100000)
+        print('等待弹窗1')
+        # 询问是否保存登陆信息 关闭
+        page.get_by_text("取消").click(timeout=100000)
 
-    print('点击私信按钮')
-    page.get_by_role("paragraph").filter(has_text="私信").click(timeout=100000)
-    print('点击续火花用户')
-    page.get_by_text(f"{config['nickname']}").click(timeout=100000)
-    print('输入文本并回车')
-    page.locator("#douyin-header-menuCt").get_by_role("textbox").locator("div").nth(2).click(timeout=100000)
-    page.locator("#douyin-header-menuCt").get_by_role("textbox").fill(f"{config['msg']}")
-    page.locator("#douyin-header-menuCt").get_by_role("textbox").press("Enter")
+        print('点击私信按钮')
+        page.get_by_role("paragraph").filter(has_text="私信").click(timeout=100000)
+        print('点击续火花用户')
+        page.get_by_text(f"{config['nickname']}").click(timeout=100000)
+        print('输入文本并回车')
+        page.locator("#douyin-header-menuCt").get_by_role("textbox").locator("div").nth(2).click(timeout=100000)
+        page.locator("#douyin-header-menuCt").get_by_role("textbox").fill(f"{config['msg']}")
+        page.locator("#douyin-header-menuCt").get_by_role("textbox").press("Enter")
 
-    page.wait_for_timeout(30000)
+        page.wait_for_timeout(30000)
 
-    print('关闭浏览器')
-    context.close()
-    browser.close()
+        print('关闭浏览器')
+        context.close()
+        browser.close()
 except Exception as e:
     # error_msg = str(e)
     error_details = traceback.format_exc()
